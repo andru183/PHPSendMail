bee063e7ca71e13c83e9e8ed7cb530d332d103aa
andru183 <andru183@gmail.com>
2015-12-08 00:12:13 +0000

    added paramaters

--- mail.php
+++ mail.php
@@ -1,10 +1,19 @@
 <?php
-$to      = 'email@address.com';
-$subject = 'sample subject';
-$message = 'hello';
-$headers = 'From: email@address.com' . "\r\n" .
-    'Reply-To: email@address.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+if($argv[1] == '--help')
+{
+	print "run the command as [mail.php to from subject message";
+}
+
+else
+{
+	$to      = $argv[1];
+	$subject = $argv[3];
+	$message = $argv[4];
+	$headers = 'From: '.$argv[2] . "\r\n" .
+	    'Reply-To: '.$argv[2]."\r\n" .
+	    'X-Mailer: PHP/' . phpversion();
+
+	mail($to, $subject, $message, $headers);
+}
 ?>
